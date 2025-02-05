e5b3f59e11ea706b2e14d865f617124f74817201
Daniil Chuhai <chugaydaniil07@gmail.com>
2025-02-05 14:19:29 +0200

    yes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/will-you-be-my-valentine",
+  base: "/will-you-be-my-valentine-main/",
   plugins: [react()],
   build: {
     outDir: 'build', // or whatever your output directory is
